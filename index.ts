1f16df59ae0742d9a714ff4933b9fbc2175cbdb9
= <=>
2024-01-28 10:24:16 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+//Use the filter method to get an array of even numbers from a given array. Utilize a lambda function as the filtering condition.
+const numbersArray: number[] = [1,2,3,4,5,6,7,8,9,10];
+ const evenNumbersArray: number[] = numbersArray.filter((number)=>number % 2 === 0);
+
+ console.log("Original Array:", numbersArray);
+ console.log("Even Numbers Array:", evenNumbersArray);
